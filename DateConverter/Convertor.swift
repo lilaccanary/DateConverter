cd125e2d6b2c68fe22a2d9bf03fecccf545750c4
AlinaKvashnina <lilaccanary@gmail.com>
2021-02-28 13:36:23 +0700

    DateConv

--- DateConverter/Convertor.swift
+++ DateConverter/Convertor.swift
@@ -16,21 +16,37 @@ class Convertor {
    
     func convertDate() -> String {
         var yearValue = ""
-        let decade = day % 10 == 0 || day == 31 ? day / 10 : day / 10 + 1
-        let monthValue = monthName[month - 1]
-        if year % 4 != 0 || year % 100 == 0 && year % 400 != 0 {
-            yearValue = " невисокосного года"
+        if day == 0 || month == 0 || year == 0 || day > 31 || month > 12 {
+            return "Введите корректные данные"
         } else {
-            yearValue = " високосного года"
+            let decade = day % 10 == 0 || day == 31 ? day / 10 : day / 10 + 1
+            let monthValue = monthName[month - 1]
+            if year % 4 != 0 || year % 100 == 0 && year % 400 != 0 {
+                yearValue = " невисокосного года"
+            } else {
+                yearValue = " високосного года"
+            }
+            return String(decade) + " декада " + String(monthValue) + yearValue
         }
-        return String(decade) + " декада " + String(monthValue) + yearValue
     }
     
     
     init(_ string: String) {
         let fullNameArr = string.split(separator: ".")
-        day = Int(fullNameArr[0])!
-        month = Int(fullNameArr[1])!
-        year = Int(fullNameArr[2])!
+        if Int(fullNameArr[0]) != nil {
+            day = Int(fullNameArr[0])!
+        } else {
+            day = 0
+        }
+        if Int(fullNameArr[1]) != nil {
+            month = Int(fullNameArr[1])!
+        } else {
+            month = 0
+        }
+        if Int(fullNameArr[2]) != nil {
+            year = Int(fullNameArr[2])!
+        } else {
+            year = 0
+        }
     }
 }
